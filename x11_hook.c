194901964a1d267acb49948b11d5d89a50ce48ca
Clément Bœsch <u@pkh.me>
2023-06-03 01:12:12 +0200

    x11: simplify atom comparison
    Atoms are unique integers which can be compared directly.

--- x11_hook.c
+++ x11_hook.c
@@ -1,6 +1,7 @@
 #include <dlfcn.h>
 #include <string.h>
 #include <X11/Xlib.h>
+#include <X11/Xatom.h>
 
 #include "wth.h"
 
@@ -16,6 +17,7 @@ typedef int (*XChangeProperty_func_type)(
 );
 
 static XChangeProperty_func_type XChangeProperty_orig;
+static Atom _NET_WM_NAME;
 
 int XChangeProperty(
     Display *display,
@@ -31,11 +33,10 @@ int XChangeProperty(
     if (!XChangeProperty_orig)
         XChangeProperty_orig = dlsym(RTLD_NEXT, __func__);
 
-    char *property_name = XGetAtomName(display, property);
-    const int chg = !strcmp(property_name, "WM_NAME") || !strcmp(property_name, "_NET_WM_NAME");
-    XFree(property_name);
+    if (!_NET_WM_NAME)
+        _NET_WM_NAME = XInternAtom(display, "_NET_WM_NAME", 0);
 
-    if (chg) {
+    if (property == XA_WM_NAME || property == _NET_WM_NAME) {
         const char *new_title = wth_get_title();
         return XChangeProperty_orig(display, w, property, type, format, mode, (const unsigned char *)new_title, (int)strlen(new_title));
     }
