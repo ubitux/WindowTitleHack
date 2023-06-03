6d77b0e99fab0c01e9b57248c97b6c913337092c
Clément Bœsch <u@pkh.me>
2023-06-03 11:07:19 +0200

    Move init once code in dedicated functions

--- x11.c
+++ x11.c
@@ -18,9 +18,20 @@ typedef int (*XChangeProperty_func_type)(
 );
 
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static int initialized;
 static const char *new_title;
 static XChangeProperty_func_type XChangeProperty_orig;
 static Atom _NET_WM_NAME;
+
+static void init_once(Display *display)
+{
+    /* Cache original function pointer */
+    XChangeProperty_orig = dlsym(RTLD_NEXT, "XChangeProperty");
+
+    _NET_WM_NAME = XInternAtom(display, "_NET_WM_NAME", 0);
+
+    new_title = wth_get_title();
+}
 
 int XChangeProperty(
     Display *display,
@@ -33,17 +44,10 @@ int XChangeProperty(
     int nelements
 ) {
     pthread_mutex_lock(&lock);
-
-    /* Cache original function pointer */
-    if (!XChangeProperty_orig)
-        XChangeProperty_orig = dlsym(RTLD_NEXT, __func__);
-
-    if (!_NET_WM_NAME)
-        _NET_WM_NAME = XInternAtom(display, "_NET_WM_NAME", 0);
-
-    if (!new_title)
-        new_title = wth_get_title();
-
+    if (!initialized) {
+        init_once(display);
+        initialized = 1;
+    }
     pthread_mutex_unlock(&lock);
 
     if (property == XA_WM_NAME || property == _NET_WM_NAME) {
--- xcb.c
+++ xcb.c
@@ -19,9 +19,24 @@ typedef xcb_void_cookie_t (*xcb_change_property_func_type)(
 );
 
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static int initialized;
 static const char *new_title;
 static xcb_change_property_func_type xcb_change_property_orig;
 static xcb_atom_t _NET_WM_NAME;
+
+static void init_once(xcb_connection_t *conn)
+{
+    /* Cache original function pointer */
+    xcb_change_property_orig = dlsym(RTLD_NEXT, "xcb_change_property");
+
+    const xcb_intern_atom_cookie_t cookie = xcb_intern_atom(conn, 0, strlen("_NET_WM_NAME"), "_NET_WM_NAME");
+    xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply(conn, cookie, NULL);
+    if (reply)
+        _NET_WM_NAME = reply->atom;
+    free(reply);
+
+    new_title = wth_get_title();
+}
 
 xcb_void_cookie_t xcb_change_property(
     xcb_connection_t *conn,
@@ -34,22 +49,10 @@ xcb_void_cookie_t xcb_change_property(
     const void *data
 ) {
     pthread_mutex_lock(&lock);
-
-    /* Cache original function pointer */
-    if (!xcb_change_property_orig)
-        xcb_change_property_orig = dlsym(RTLD_NEXT, __func__);
-
-    if (!_NET_WM_NAME) {
-        const xcb_intern_atom_cookie_t cookie = xcb_intern_atom(conn, 0, strlen("_NET_WM_NAME"), "_NET_WM_NAME");
-        xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply(conn, cookie, NULL);
-        if (reply)
-            _NET_WM_NAME = reply->atom;
-        free(reply);
+    if (!initialized) {
+        init_once(conn);
+        initialized = 1;
     }
-
-    if (!new_title)
-        new_title = wth_get_title();
-
     pthread_mutex_unlock(&lock);
 
     if (property == XCB_ATOM_WM_NAME || property == _NET_WM_NAME) {
