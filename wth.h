c0841b646bba70623bef2c1e95855903e9e0e361
Clément Bœsch <u@pkh.me>
2023-06-03 11:34:03 +0200

    Reorganize header declarations

--- wth.h
+++ wth.h
@@ -3,7 +3,7 @@
 
 #include <pthread.h>
 
-typedef void (*init_func_type)(void *user_arg);
+const char *wth_get_title(void);
 
 struct wth_once {
     pthread_mutex_t lock;
@@ -12,7 +12,7 @@ struct wth_once {
 
 #define WTH_ONCE_INITIALIZER {.lock=PTHREAD_MUTEX_INITIALIZER}
 
-const char *wth_get_title(void);
+typedef void (*init_func_type)(void *user_arg);
 void wth_init_once(struct wth_once *once, init_func_type init_func, void *user_arg);
 
 #endif
